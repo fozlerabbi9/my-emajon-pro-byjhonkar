d71170f66f0fc9b0389a806fbb2cc15b516b3431
Fozle Rabbi <fozlerabbi9790@gmail.com>
2022-03-23 20:06:00 +0600

    add info-cart

--- src/AllComponents/ShopFile/ProductInfo.js
+++ src/AllComponents/ShopFile/ProductInfo.js
@@ -3,17 +3,26 @@ import './ProductInfo.css';
 
 const ProductInfo = (props) => {
     const { cart } = props;
+    console.log(cart)
 
     let totalPrice = 0;
+    let shipping = 0;
     for (let product of cart) {
         totalPrice = totalPrice + product.price;
+        shipping = shipping + product.shipping;
     }
+    const tax = (totalPrice * 5 / 100).toFixed(2);
+    const grandTotal = totalPrice + shipping + parseInt(tax);
 
     return (
         <div>
             <h4>order summary</h4>
-            <p>cart length {cart.length}</p>
-            <p>Total Price : {totalPrice}</p>
+            <p>cart length : {cart.length}</p>
+            <p>Total Price : ${totalPrice}</p>
+            <p>Shipping : ${shipping}</p>
+            <p>Tax : ${tax}</p>
+            <p>Grand Total $ : {grandTotal} </p>
+
         </div>
     );
 };
